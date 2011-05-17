b42ceb73de342a4836838915f8cb3d4640b5d7d0
Travis Wyatt <travis.i.wyatt@gmail.com>
2011-05-16 19:56:44 -0700

    Added skeleton credits figure and am now seeding random number generator.

---
+++ callbackCredits.m
@@ -0,0 +1,1 @@
+openfig('credits.fig');
--- initGame.m
+++ initGame.m
@@ -1,4 +1,4 @@
-% TODO: seed random number generator
+rand('seed', 1000 * sum(clock));
 
 global TYPE;
 TYPE = struct('HUMAN_VS_HUMAN', 1, 'EASY', 2, 'MODERATE', 3, 'EXPERT', 4);
