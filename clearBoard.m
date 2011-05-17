c580ea1b431bf84c4e57ecc6229665aef3fc57fb
Travis Wyatt <travis.i.wyatt@gmail.com>
2011-05-17 01:03:55 -0700

    Fixed clearing of board at end of a game causing the board to clear again on next click.

--- clearBoard.m
+++ clearBoard.m
@@ -5,6 +5,8 @@ board = zeros(3);
 boardRender(board);
 boardColor(board);
 
+clearBoardOnNextClick = false; % boolean to have next click clear the game board
+
 disp('Setting turn to player X.');
 % set player 1 to go first
 whosTurn = 1;
